993a7d305f11178831614feed80403ae0de42d9e
Darkpfeffer <darkpfeffer97@gmail.com>
2023-12-10 18:55:34 +0100

    Created chart types

--- src/recipes/forms.py
+++ src/recipes/forms.py
@@ -6,6 +6,14 @@ SEARCH_MODEL_CHOICES = (
     ('#2', 'Ingredients')
 )
 
+CHART_CHOICES = (
+    ('#1', 'Bar chart (Recipe)'),
+    ('#2', 'Pie chart (Recipe)'),
+    ('#3', 'Line chart (Ingredient)'),
+    ('#4', 'None')
+)
+
 class SearchForm(forms.Form):
     search_criteria = forms.CharField(max_length = 200, required=False)
     model_choice = forms.ChoiceField(choices = SEARCH_MODEL_CHOICES)
+    chart_type = forms.ChoiceField(choices= CHART_CHOICES)
--- src/recipes/utils.py
+++ src/recipes/utils.py
@@ -1,7 +1,13 @@
 from django.shortcuts import reverse
+from io import BytesIO
+import base64
+import matplotlib.pyplot as plt
+#Import function and models from Project
 from users.models import User
 from .models import Recipe
 from ingredients.models import Ingredient
+
+#Write your functions here:
 
 def get_username_from_id(val):
     username = User.objects.get(id = val).username
@@ -19,3 +25,50 @@ def make_clickable_ingredient(val):
     ingredientURL = ingredient.get_absolute_url()
 
     return ingredientURL
+
+def get_graph():
+    buffer = BytesIO()
+
+    #Save plot as file-like object
+    plt.savefig(buffer, format='png')
+
+    #set cursor back to the beginning
+    buffer.seek(0)
+
+    image_png = buffer.getvalue()
+
+    graph = base64.b64encode(image_png).decode('utf-8')
+
+    buffer.close()
+
+    return graph
+
+def get_chart(chart_type, model_choice, data, **kwargs):
+    plt.switch_backend('AGG')
+
+    #specify figure size
+    fig = plt.figure(figsize = (6,3))
+
+    user_occurrence = Recipe.objects.filter(creator_id_id = data['creator_id_id']).count()
+    ingredient_occurrence = Ingredient.objects.get(id = data['id']).recipe_appearance.count()
+
+    if chart_type == '#1' and model_choice == '#1':
+        plt.bar(data['creator_id_username'], data[user_occurrence])
+
+    elif chart_type == '#2' and model_choice == '#2':
+        labels=kwargs.get('labels')
+        plt.pie(data[ingredient_occurrence], labels=labels)
+
+    elif chart_type == '#3' and model_choice == '#1':
+        plt.plot(data['name'], data['cooking_time'])
+
+    elif chart_type == '#4':
+        print('No chart type chosen.')
+
+    else:
+        print('Unknown chart type.')
+
+    plt.tight_layout()
+
+    chart = get_graph()
+    return chart
