e8929f1294ac23b046ef72a5dd1e953da523d5e0
Darkpfeffer <darkpfeffer97@gmail.com>
2024-02-04 19:49:55 +0100

    Renamed magnifying glass class to diagram

--- recipe_project/forms.py
+++ recipe_project/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from PIL import Image
+from django.core.exceptions import ValidationError
+
+def validate_image(file):
+    try:
+        Image.open(file)
+        return file
+    except:
+        raise ValidationError("File is not supported")
 
 class RegisterUserForm(forms.Form):
     username = forms.CharField(max_length=120)
     password = forms.CharField(widget=forms.PasswordInput)
     email = forms.EmailField()
-    profile_pic = forms.ImageField(required=False)
+    profile_pic = forms.ImageField(required=False, validators=[validate_image])
--- recipe_project/tests.py
+++ recipe_project/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from .forms import RegisterUserForm
+from .forms import RegisterUserForm, validate_image
+from django.core.exceptions import ValidationError
+from django.conf import settings
 import tempfile
 
 class RegisterUserFormTest(TestCase):
@@ -63,3 +65,23 @@ class RegisterUserFormTest(TestCase):
 
         self.assertFalse(form.is_valid())
 
+    def test_register_form_pic_is_not_valid(self):
+        form = RegisterUserForm(
+            data={
+                'username': 'Test123',
+                'password': '123456test',
+                'email': 'testuser@gmail.com',
+                'profile_pic': 'no_picture.jpg'
+            }
+        )
+
+        self.assertTrue(form.is_valid())
+
+    def test_validate_image_fail(self):
+        with self.assertRaises(ValidationError):
+            validate_image('asd')
+
+    def test_validate_image_pass(self):
+        pic = settings.BASE_DIR / 'media' / 'no_picture.jpg'
+        results = validate_image(pic)
+        self.assertIsNotNone(results)
