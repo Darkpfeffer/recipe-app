723509bf280a14357be149bf19c9069149351f4e
Darkpfeffer <darkpfeffer97@gmail.com>
2024-02-05 01:28:16 +0100

    Production ready

--- recipe_project/settings/prod.py
+++ recipe_project/settings/prod.py
@@ -1,2 +1,25 @@
 from .base import *
+import os
+import dj_database_url
 
+SECRET_KEY = os.environ.get("SECRET_KEY_ENV")
+
+DATABASES = {
+	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_SSL_REDIRECT = True
+
+SECURE_HSTS_SECONDS = 60
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+SECURE_HSTS_PRELOAD = True
