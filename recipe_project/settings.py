de7cef6a2743fd8ba9cd9661e8453b0c82a19c7f
Darkpfeffer <darkpfeffer97@gmail.com>
2024-02-07 11:23:06 +0100

    Testing default whitestorage configuration

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -101,7 +101,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage" #whitenoise.storage.CompressedStaticFilesStorage
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" #whitenoise.storage.CompressedStaticFilesStorage
 #whitenoise.storage.CompressedManifestStaticFilesStorage
 
 #AUTH
